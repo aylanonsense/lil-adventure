1c972f0e806dec3ed1597dbc62e9cccde0abfef0
bridgs <bridgs.dev@gmail.com>
2019-02-02 13:46:54 -0500

    Tiles

--- main.lua
+++ main.lua
@@ -1,11 +1,35 @@
 -- Constants
-local GAME_WIDTH = 200
-local GAME_HEIGHT = 200
+local GAME_WIDTH = 192
+local GAME_HEIGHT = 192
 local RENDER_SCALE = 3
-local TILE_SIZE = 16
+local LEVEL_WIDTH = 12
+local LEVEL_HEIGHT = 12
+local LEVEL_DATA = [[
+TT.TTTTT....
+T......T....
+T......T....
+T......T....
+T......T....
+T......T....
+T......T....
+TTTTTTTT....
+TTTTTTTT....
+TTTTTTTT....
+TTTTTTTT....
+TTTTTTTT....
+]]
+local TILE_TYPES = {
+  -- Grass
+  ['.'] = { sprite = 1 },
+  -- Tree
+  ['T'] = { sprite = 2, isImpassable = true }
+}
 
 -- Game objects
 local player
+
+-- Tile grid
+local tileGrid
 
 -- Images
 local playerImage
@@ -17,7 +41,10 @@ function love.load()
   playerImage = loadImage('img/player.png')
   tilesImage = loadImage('img/tiles.png')
 
-  -- Create the player characters
+  -- Create the level
+  createTileGrid()
+
+  -- Create the player character
   createPlayer()
 end
 
@@ -36,16 +63,70 @@ function love.draw()
   love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
   love.graphics.setColor(1, 1, 1, 1)
 
+  -- Draw the tiles
+  for col = 1, LEVEL_WIDTH do
+    for row = 1, LEVEL_HEIGHT do
+      local tile = tileGrid[col][row]
+      if tile then
+        drawImage(tilesImage, tile.sprite, calculateRenderPosition(col, row))
+      end
+    end
+  end
+
   -- Draw the player
   drawImage(playerImage, 1, calculateRenderPosition(player.col, player.row))
+end
+
+-- Move the player when a button is pressed
+function love.keypressed(key)
+  -- Figure out which tile is being moved into
+  local col = player.col
+  local row = player.row
+  if key == 'up' then
+    row = row - 1
+  elseif key == 'down' then
+    row = row + 1
+  elseif key == 'left' then
+    col = col - 1
+  elseif key == 'right' then
+    col = col + 1
+  end
+  -- Figure out if the player can move into that tile
+  local canMoveIntoTile = true
+  if col < 1 or col > LEVEL_WIDTH or row < 1 or row > LEVEL_HEIGHT then
+    canMoveIntoTile = false
+  else
+    local tile = tileGrid[col][row]
+    if tile and tile.isImpassable then
+      canMoveIntoTile = false
+    end
+  end
+  -- Move the player
+  if canMoveIntoTile then
+    player.col = col
+    player.row = row
+  end
 end
 
 -- Creates the player
 function createPlayer()
   player = {
-    col = 3,
-    row = 1
+    col = 5,
+    row = 5
   }
+end
+
+-- Create a 2D grid of tiles
+function createTileGrid()
+  tileGrid = {}
+  for col = 1, LEVEL_WIDTH do
+    tileGrid[col] = {}
+    for row = 1, LEVEL_HEIGHT do
+      local i = (LEVEL_HEIGHT + 1) * (row - 1) + col
+      local symbol = string.sub(LEVEL_DATA, i, i)
+      tileGrid[col][row] = TILE_TYPES[symbol]
+    end
+  end
 end
 
 -- Loads a pixelated image
@@ -55,17 +136,16 @@ function loadImage(filePath)
   return image
 end
 
--- Draws a 16x16 sprite from sprite sheet, 1 is the upper-leftmost sprite
+-- Draws a 16x16 sprite from an image, spriteNum=1 is the upper-leftmost sprite
 function drawImage(image, spriteNum, x, y)
-  local columns = math.floor(image:getWidth() / TILE_SIZE)
+  local columns = math.floor(image:getWidth() / 16)
   local col = (spriteNum - 1) % columns
   local row = math.floor((spriteNum - 1) / columns)
-  local quad = love.graphics.newQuad(TILE_SIZE * col, TILE_SIZE * row, TILE_SIZE, TILE_SIZE, image:getDimensions())
+  local quad = love.graphics.newQuad(16 * col, 16 * row, 16, 16, image:getDimensions())
   love.graphics.draw(image, quad, x, y)
 end
 
--- Takes in a col and a row integer and returns x,y coordinates
+-- Takes in a column and a row and returns the corresponding x,y coordinates
 function calculateRenderPosition(col, row)
-  return TILE_SIZE * col, TILE_SIZE * row
+  return 16 * (col - 1), 16 * (row - 1)
 end
-
