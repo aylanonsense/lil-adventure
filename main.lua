b6801d696f5e8efe4777c5ebf994c9f462be2e99
bridgs <bridgs.dev@gmail.com>
2019-02-02 12:49:41 -0500

    A silly player object

--- main.lua
+++ main.lua
@@ -2,16 +2,30 @@
 local GAME_WIDTH = 200
 local GAME_HEIGHT = 200
 local RENDER_SCALE = 3
+local TILE_SIZE = 16
 
--- love.load is called once when our game loads
+-- Game objects
+local player
+
+-- Images
+local playerImage
+local tilesImage
+
+-- Initializes the game
 function love.load()
+  -- Load images
+  playerImage = loadImage('img/player.png')
+  tilesImage = loadImage('img/tiles.png')
+
+  -- Create the player characters
+  createPlayer()
 end
 
--- love.update is called 60 time each second, it's here we update our game
+-- Updates the game state
 function love.update(dt)
 end
 
--- love.draw is called after love.update, we just render the game here
+-- Renders the game
 function love.draw()
   -- Set some drawing filters
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -21,4 +35,37 @@ function love.draw()
   love.graphics.setColor(0, 0, 0, 1)
   love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
   love.graphics.setColor(1, 1, 1, 1)
+
+  -- Draw the player
+  drawImage(playerImage, 1, calculateRenderPosition(player.col, player.row))
 end
+
+-- Creates the player
+function createPlayer()
+  player = {
+    col = 3,
+    row = 1
+  }
+end
+
+-- Loads a pixelated image
+function loadImage(filePath)
+  local image = love.graphics.newImage(filePath)
+  image:setFilter('nearest', 'nearest')
+  return image
+end
+
+-- Draws a 16x16 sprite from sprite sheet, 1 is the upper-leftmost sprite
+function drawImage(image, spriteNum, x, y)
+  local columns = math.floor(image:getWidth() / TILE_SIZE)
+  local col = (spriteNum - 1) % columns
+  local row = math.floor((spriteNum - 1) / columns)
+  local quad = love.graphics.newQuad(TILE_SIZE * col, TILE_SIZE * row, TILE_SIZE, TILE_SIZE, image:getDimensions())
+  love.graphics.draw(image, quad, x, y)
+end
+
+-- Takes in a col and a row integer and returns x,y coordinates
+function calculateRenderPosition(col, row)
+  return TILE_SIZE * col, TILE_SIZE * row
+end
+
